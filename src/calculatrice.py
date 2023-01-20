17147bf9f661c0863dae0e3560829ac942357be2
stephane447 <stephane.menga-kamdjo@ecole-it.com>
2023-01-20 10:17:47 +0100

    ajout de src/calculatrice.py et src/main.py

---
+++ src/calculatrice.py
@@ -0,0 +1,15 @@
+def multiplication(x: float, y: float) -> float:
+
+return x * y
+
+def division(x: float, y: float) -> float:
+
+return x / y
+
+def addition(x: float, y: float) -> float:
+
+return x + y
+
+def subtraction(x: float, y: float) -> float:
+
+return x - y
